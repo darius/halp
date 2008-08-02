2c64c2401ca4bb535938528fd13e33ea0a99cb6a
darius <darius@66-214-133-204.dhcp.gldl.ca.charter.com>
2008-08-02 15:44:23 -0700

    untabify

--- halp.el
+++ halp.el
@@ -13,13 +13,13 @@
   (let ((output (halp-get-output-buffer)))
 ;;    (call-process-region (point-min) (point-max) "cat" t t)
     (let ((rc (call-process-region (point-min) (point-max)
-				   command nil output)))
-      (cond ((zerop rc)			;success
-	     (halp-update-current-buffer output)
-	     (message "hooray"))
-	    ((numberp rc)
-	     (message "Helper process failed"))
-	    (t (message rc))))))
+                                   command nil output)))
+      (cond ((zerop rc)                 ;success
+             (halp-update-current-buffer output)
+             (message "hooray"))
+            ((numberp rc)
+             (message "Helper process failed"))
+            (t (message rc))))))
 
 (defun halp-get-output-buffer ()
   "Return an empty buffer dedicated (hopefully) to halp's use."
@@ -43,16 +43,16 @@
 
 (defun halp-add-hook (hook map-name key helper-command)
   (add-hook hook
-	    `(lambda ()
-	       (define-key ,map-name ',key
-		 (lambda ()
-		   (interactive)
-		   (halp-update ',helper-command))))))
+            `(lambda ()
+               (define-key ,map-name ',key
+                 (lambda ()
+                   (interactive)
+                   (halp-update ',helper-command))))))
 
 (halp-add-hook 'sh-mode-hook 'sh-mode-map
-	       "\M-i" (concat halp-helpers-directory "sh-halp.sh"))
+               "\M-i" (concat halp-helpers-directory "sh-halp.sh"))
 
 (halp-add-hook 'haskell-mode-hook 'haskell-mode-map
-	       "\M-i" (concat halp-helpers-directory "ghci-halp.sh"))
+               "\M-i" (concat halp-helpers-directory "ghci-halp.sh"))
 
 (provide 'halp)
