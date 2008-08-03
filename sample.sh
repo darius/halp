74d9c4acd45bc9dc2ba00dd258fe574777e68518
darius <darius@66-214-133-204.dhcp.gldl.ca.charter.com>
2008-08-02 22:41:34 -0700

    get sh-halp.sh to actually work again (once again, in a still-fragile way)

--- sample.sh
+++ sample.sh
@@ -1,6 +1,5 @@
-Fafblog rules ok.
+When you hit M-i you should see '| 5' appear below the following line:
 
 $ awk 'BEGIN { print 2+3 }'
-| 5
 
-here we go
+Yay!
--- sh-halp.sh
+++ sh-halp.sh
@@ -3,14 +3,18 @@
 # Distributed under the terms of the MIT X License, found at
 # http://www.opensource.org/licenses/mit-license.php
 
-work=`mktemp -d`
+work=`mktemp -d /tmp/halp.XXXXXXXXXX`
 touch ${work}/line_numbers
 
 grep -v '^| ' >${work}/input
 awk '/^[$]/ { print NR >"'${work}/line_numbers'";
               print "echo -n SEP;" substr($0, 2); }' ${work}/input |
   sh | sed 's/^SEP/| /' |
-  paste -d'a' ${work}/line_numbers - >${work}/edits
+  awk '
+    { getline linenumber <"'${work}/line_numbers'";
+      print linenumber "a \\";
+      print $0; }' >${work}/edits
+
 sed -f ${work}/edits <${work}/input
 
 rm -r ${work}
