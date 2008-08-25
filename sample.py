77034309ee9929ca4a759185b2489566e9a79033
darius <darius@68-190-209-27.dhcp.gldl.ca.charter.com>
2008-08-24 18:50:42 -0700

    get sample code to run in older python versions

--- sample.py
+++ sample.py
@@ -3,7 +3,10 @@
 import operator
 
 def fact(n):
-    return 1 if n <= 0 else n * fact(n - 1)
+    if n <= 0:
+        return 1
+    else:
+        return n * fact(n - 1)
 
 # When you hit M-i you should see '#| 120' appear below the following line:
 
