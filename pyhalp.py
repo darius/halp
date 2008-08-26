c2d16f7ad1808ae181bb38da3dc5d50596484951
darius <darius@darius-bacons-macbook.local>
2008-08-26 12:26:53 -0700

    drop topmost frame from tracebacks (it's from pyhalp internals)

--- pyhalp.py
+++ pyhalp.py
@@ -12,6 +12,20 @@ dbg = False
 def format_result(s):
     return '#| %s' % s.replace('\n', '\n#| ')
 
+def format_exc(limit=None):
+    """Like traceback.format_exc() but I'll be fiddling with it."""
+    try:
+        etype, value, tb = sys.exc_info()
+        lines = format_exception(etype, value, tb, limit)
+        return ''.join(lines)
+    finally:
+        etype = value = tb = None
+
+def format_exception(etype, value, tb, limit=None):
+     return (['Traceback (most recent call last):\n']
+             + traceback.format_tb(tb, limit)[1:]
+             + traceback.format_exception_only(etype, value))
+
 def get_lineno((etype, value, tb)):
     if isinstance(value, SyntaxError) and value.filename == '<string>':
         return value.lineno
@@ -31,7 +45,7 @@ try:
     exec '\n'.join(input) in mod_dict
 except:
     lineno = get_lineno(sys.exc_info())
-    input.insert(lineno, format_result(traceback.format_exc()))
+    input.insert(lineno, format_result(format_exc()))
     sys.stdout.write('\n'.join(input))
     sys.exit(0)
 
@@ -45,9 +59,9 @@ def interpret(code):
             exec code in mod_dict
             return None
         except:
-            return traceback.format_exc()
+            return format_exc()
     except:
-        return traceback.format_exc()
+        return format_exc()
 
 output = []
 for line in input:
