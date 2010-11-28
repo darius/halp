0d472046147d3133cf17486a62ae97acfaa79eb8
Darius Bacon <darius@wry.me>
2010-11-27 20:48:02 -0800

    in test, expect no ## line in traceback

--- tests/expected.sample.py
+++ tests/expected.sample.py
@@ -1,9 +1,7 @@
 43 1 1
 #. 2
-30 0 6
+30 0 4
 #. Traceback (most recent call last):
-#.   File "<string>", line 34, in <module>
-#.     ## fact('notanumber')
 #.   File "<string>", line 7, in fact
 #.     return n * fact(n - 1)
 #. TypeError: unsupported operand type(s) for -: 'str' and 'int'
